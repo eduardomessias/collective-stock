a43eee183f6cd342ff70b25b9ead99ccc91dced6
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2021-07-09 19:13:18 +0000

    ebay notifications

--- api/ebay/notifications.py
+++ api/ebay/notifications.py
@@ -27,6 +27,6 @@ class handler(BaseHTTPRequestHandler):
 
 
     self.send_response(200)
-    self.send_header('Content-type', 'application/json')
+    #self.send_header('Content-type', 'application/json')
     self.end_headers()
     self.wfile.write(str(json_string.encode('utf-8')))
