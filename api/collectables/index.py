60c545bbdc0ee8679762ac5300dbe005078811e4
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2021-07-09 09:08:16 +0000

    ebay sdk

---
+++ api/collectables/index.py
@@ -0,0 +1,42 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+
+  def do_GET(self):
+    import datetime
+    import json
+
+    from ebaysdk.exception import ConnectionError
+    from ebaysdk.finding import Connection as Finding
+
+    try:
+        ebay = Finding (domain='svcs.sandbox.ebay.com', appid='EduardoM-collecti-SBX-d583e0402-1c822ca4', config_file=None)
+
+        response = ebay.execute('findItemsAdvanced', {
+            'keywords': 'Collectibles',
+            #'categoryId' : ['177', '111422'],
+            # 'itemFilter': [
+            #     {'name': 'Condition', 'value': 'Used'},
+            #     {'name': 'MinPrice', 'value': '200', 'paramName': 'Currency', 'paramValue': 'GBP'},
+            #     {'name': 'MaxPrice', 'value': '400', 'paramName': 'Currency', 'paramValue': 'GBP'}
+            # ],
+            # 'paginationInput': {
+            #     'entriesPerPage': '25',
+            #     'pageNumber': '1' 	 
+            # },
+            'sortOrder': 'CurrentPriceHighest'
+        })
+        count = int(response.reply.searchResult._count)
+
+        if count > 0:
+            item_list = response.reply.searchResult.item
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(str([item.title for item in item_list]).encode())
+    except ConnectionError as e:
+        self.send_response(500)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(e).encode())
