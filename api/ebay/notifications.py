19ac1cb257f5cc4b45eb8bea4cbfbf95e2718194
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2021-07-09 17:57:28 +0000

    ebay notifications

--- api/ebay/notifications.py
+++ api/ebay/notifications.py
@@ -2,6 +2,12 @@ from http.server import BaseHTTPRequestHandler
 
 
 class handler(BaseHTTPRequestHandler):
+  def __init__(self, challenge_code):
+    self.challenge_code = challenge_code
+
+  def __call__(self, *args, **kwargs):
+    """ Handle a request """
+    super().__init__(*args, **kwargs)
 
 
   def do_GET(self, challenge_code):
@@ -11,7 +17,7 @@ class handler(BaseHTTPRequestHandler):
 
     verification_token = 'KpFTCRfbImSLyLxKHyE4abXo4iywHI9q'
     endpoint = 'https://collective-stock.vercel.app/api/ebay/notifications'
-    m = hashlib.sha256(str(challenge_code+verification_token+endpoint).encode())
+    m = hashlib.sha256(str(self.challenge_code+verification_token+endpoint).encode())
     hash = m.hexdigest()
 
     self.send_response(200)
