38e3305e0669c0356c2e21f111f7b42b52fd7426
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2021-07-19 23:21:42 +0000

    refactoring

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,24 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 
 class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const originalRenderPage = ctx.renderPage
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                // useful for wrapping the whole react tree
+                enhanceApp: (App) => App,
+                // useful for wrapping in a per-page basis
+                enhanceComponent: (Component) => Component,
+            })
+
+        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+
     render() {
         return (
             <Html>
@@ -17,6 +35,5 @@ class MyDocument extends Document {
     }
 }
 
-  
+
 export default MyDocument
-  
