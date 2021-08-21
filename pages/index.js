4f574506f064647052b1004ed09240258d56319f
Eduardo Messias Andrade e Oliveira <eduardomessiasandradeeoliveira@Eduardos-MacBook-Pro.local>
2021-08-21 15:03:20 +0100

    payload

--- pages/index.js
+++ pages/index.js
@@ -1,191 +1,92 @@
-import React, { Component } from 'react'
 import Image from 'next/image'
 
-import {
-  Body,
-  TopContainer,
-  Sidebar,
-  Overlay,
-  PageContent,
-  Header,
-  Footer
-} from '../components/layout'
+import { useState } from 'react'
 
-import InfoCardSection from '../components/infoCardSection'
-import VolumeSection from '../components/volumeSection'
+import { Body, TopContainer, Sidebar, Overlay, PageContent, Header, InfoCard } from '../components/layout'
 
-class Home extends Component {
-  constructor (props) {
-    super (props)
-
-    this.state = {
-      showSidebar: false,
-      showOverlay: false
-    }
-    this.toggleSidebar = this.toggleSidebar.bind (this)
-  }
-
-  toggleSidebar () {
-    this.setState ({ showSidebar: !this.state.showSidebar, showOverlay: !this.state.showOverlay })
-  }
-
-  render = () =>
-    <Body>
-      <TopContainer toggleSidebar={this.toggleSidebar} />
-      <Sidebar showSidebar={this.state.showSidebar} toggleSidebar={this.toggleSidebar} />
-      <Overlay showOverlay={this.state.showOverlay} toggleSidebar={this.toggleSidebar} />
-  
-      <PageContent>
-  
-        <Header />
-  
-        <InfoCardSection />
-  
-        <VolumeSection />
-  
-        <hr />
-  
-        <div className="w3-container">
-          <h5>General Stats</h5>
-          <p>New Visitors</p>
-          <div className="w3-grey">
-            <div className="w3-container w3-center w3-padding w3-green" style={{ width: 25 + '%' }}>+25%</div>
-          </div>
-  
-          <p>New Users</p>
-          <div className="w3-grey">
-            <div className="w3-container w3-center w3-padding w3-orange" style={{ width: 50 + '%' }}>50%</div>
-          </div>
-  
-          <p>Bounce Rate</p>
-          <div className="w3-grey">
-            <div className="w3-container w3-center w3-padding w3-red" style={{ width: 75 + '%' }}>75%</div>
-          </div>
-        </div>
-  
-        <hr />
-  
-        <div className="w3-container">
-          <h5>Countries</h5>
-          <table className="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>United States</td>
-                <td>65%</td>
-              </tr>
-              <tr>
-                <td>UK</td>
-                <td>15.7%</td>
-              </tr>
-              <tr>
-                <td>Russia</td>
-                <td>5.6%</td>
-              </tr>
-              <tr>
-                <td>Spain</td>
-                <td>2.1%</td>
-              </tr>
-              <tr>
-                <td>India</td>
-                <td>1.9%</td>
-              </tr>
-              <tr>
-                <td>France</td>
-                <td>1.5%</td>
-              </tr>
-            </ tbody>
-          </table>
-          <br />
-          <button className="w3-button w3-dark-grey">More Countries  <i className="fa fa-arrow-right"></i></button>
-        </div>
-  
-        <hr />
-  
-        <div className="w3-container">
-          <h5>Recent Users</h5>
-          <ul className="w3-ul w3-card-4 w3-white">
-            <li className="w3-padding-16">
-              <Image alt="" src="/w3images/avatar2.png" className="w3-left w3-circle w3-margin-right" width={35} height={35} />
-              <span className="w3-xlarge">Mike</span>
-              <br />
-            </li>
-            <li className="w3-padding-16">
-              <Image alt="" src="/w3images/avatar5.png" className="w3-left w3-circle w3-margin-right" width={35} height={35} />
-              <span className="w3-xlarge">Jill</span>
-              <br />
-            </li>
-            <li className="w3-padding-16">
-              <Image alt="" src="/w3images/avatar6.png" className="w3-left w3-circle w3-margin-right" width={35} height={35} />
-              <span className="w3-xlarge">Jane</span>
-              <br />
-            </li>
-          </ul>
-        </div>
-  
-        <hr />
-  
-        <div className="w3-container">
-          <h5>Recent Comments</h5>
-          <div className="w3-row">
-            <div className="w3-col m2 text-center">
-              <Image alt="" className="w3-circle" src="/w3images/avatar3.png" width={96} height={96} />
-            </div>
-            <div className="w3-col m10 w3-container">
-              <h4>John <span className="w3-opacity w3-medium">Sep 29, 2014, 9:12 PM</span></h4>
-              <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <br />
-            </div>
-          </div>
-  
-          <div className="w3-row">
-            <div className="w3-col m2 text-center">
-              <Image alt="" className="w3-circle" src="/w3images/avatar1.png" width={96} height={96} />
-            </div>
-            <div className="w3-col m10 w3-container">
-              <h4>Bo <span className="w3-opacity w3-medium">Sep 28, 2014, 10:15 PM</span></h4>
-              <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <br />
-            </div>
-          </div>
-        </div>
-  
-        <br />
-  
-        <div className="w3-container w3-dark-grey w3-padding-32">
-          <div className="w3-row">
-            <div className="w3-container w3-third">
-              <h5 className="w3-bottombar w3-border-green">Demographic</h5>
-              <p>Language</p>
-              <p>Country</p>
-              <p>City</p>
-            </div>
-            <div className="w3-container w3-third">
-              <h5 className="w3-bottombar w3-border-red">System</h5>
-              <p>Browser</p>
-              <p>OS</p>
-              <p>More</p>
-            </div>
-            <div className="w3-container w3-third">
-              <h5 className="w3-bottombar w3-border-orange">Target</h5>
-              <p>Users</p>
-              <p>Active</p>
-              <p>Geo</p>
-              <p>Interests</p>
-            </div>
-          </div>
-        </div>
-  
-        <Footer />
-  
-      </PageContent>
-    </Body>
-    
+const user = {
+    name: 'Eduardo',
+    avatar: '/images/profile.jpeg'
 }
 
-export default Home
+export default function Dashboard () {
+    const [sidebarVisibility,setSidebarVisibility] = useState (false)
+    const [sidebarOverlay,setSidebarOverlay] = useState (false)
+
+    function toggleSidebar () {
+        setSidebarVisibility (!sidebarVisibility)
+        setSidebarOverlay (!sidebarOverlay)
+    }
+
+
+    return (
+        <Body>
+
+            {/* <!-- Top container --> */}
+            <TopContainer>
+                <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={toggleSidebar}><i className="fa fa-bars"></i>  Menu</button>
+                <span className="w3-bar-item w3-right">Collective-Stock</span>
+            </TopContainer>
+
+            {/* <!-- Sidebar/menu --> */}
+            <Sidebar visible={sidebarVisibility}>
+                <br />
+                <div className="w3-container w3-row">
+                    <div className="w3-col s4">
+                        <Image alt="" src={user.avatar} className="w3-circle" width={46} height={46} />
+                    </div>
+                    <div className="w3-col s8 w3-bar">
+                        <span>Welcome, <strong>{user.name}</strong></span>
+                        <br />
+                        <a href="#" className="w3-bar-item w3-button"><i className="fa fa-envelope"></i></a>
+                        <a href="#" className="w3-bar-item w3-button"><i className="fa fa-user"></i></a>
+                        <a href="#" className="w3-bar-item w3-button"><i className="fa fa-cog"></i></a>
+                    </div>
+                </div>
+                <hr />
+                <div className="w3-container">
+                    <h5>Dashboard</h5>
+                </div>
+                <div className="w3-bar-block">
+                    <a href="#" className="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onClick={toggleSidebar} title="close menu"><i className="fa fa-remove fa-fw"></i>  Close Menu</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding w3-blue"><i className="fa fa-users fa-fw"></i>  Overview</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-eye fa-fw"></i>  Views</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-users fa-fw"></i>  Traffic</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bullseye fa-fw"></i>  Geo</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-diamond fa-fw"></i>  Orders</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bell fa-fw"></i>  News</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-bank fa-fw"></i>  General</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-history fa-fw"></i>  History</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding"><i className="fa fa-cog fa-fw"></i>  Settings</a>
+                    <br />
+                    <br />
+                </div>
+            </Sidebar>
+
+            {/* <!-- Sidebar overlay --> */}
+            <Overlay overlay={sidebarOverlay} onClick={toggleSidebar} />
+
+            {/* <!-- !PAGE CONTENT! --> */}
+            <PageContent>
+                <Header caption='My collection' icon='diamond' />
+
+                <div className="w3-row-padding w3-margin-bottom">
+                    <div className="w3-quarter">
+                        <InfoCard caption="Commics" value="52" color="white" icon="comment" img="comics.jpeg" />
+                    </div>
+                    <div className="w3-quarter">
+                        <InfoCard caption="Watches" value="99" color="white" icon="clock-o" img="watches.jpg" />
+                    </div>
+                    <div className="w3-quarter">
+                        {/* <InfoCard caption="Shares" value="23" color="teal" icon="share-alt" image="" /> */}
+                    </div>
+                    <div className="w3-quarter">
+                        {/* <InfoCard caption="Users" value="358" color="orange" icon="users" image="" /> */}
+                    </div>
+                </div>
+
+            </PageContent>
+
+        </Body>
+    )
+}
